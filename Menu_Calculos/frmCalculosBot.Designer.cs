6499a2ba0a81aa07178f93cb1001187b84e66bc0
LabInfo <LabInfo@ANEXOA2M10>
2026-02-24 12:17:25 -0300

    Adicionar arquivos de projeto.

---
+++ Menu_Calculos/frmCalculosBot.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Menu_Calculos
+{
+    partial class frmCalculosBot
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.lblSinal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.btnLimparTexto = new System.Windows.Forms.Button();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtN1
+            // 
+            this.txtN1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN1.Location = new System.Drawing.Point(333, 68);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 22);
+            this.txtN1.TabIndex = 0;
+            this.txtN1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtN2.Location = new System.Drawing.Point(333, 123);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 22);
+            this.txtN2.TabIndex = 1;
+            this.txtN2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtN2.TextChanged += new System.EventHandler(this.txtN2_TextChanged);
+            // 
+            // lblSinal
+            // 
+            this.lblSinal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSinal.Location = new System.Drawing.Point(287, 123);
+            this.lblSinal.Name = "lblSinal";
+            this.lblSinal.Size = new System.Drawing.Size(26, 32);
+            this.lblSinal.TabIndex = 2;
+            this.lblSinal.Text = "?";
+            this.lblSinal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(330, 184);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "____________________";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(333, 241);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(100, 23);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "?";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSomar.Location = new System.Drawing.Point(173, 503);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(75, 23);
+            this.btnSomar.TabIndex = 5;
+            this.btnSomar.Text = "&Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // btnLimparTexto
+            // 
+            this.btnLimparTexto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimparTexto.Location = new System.Drawing.Point(409, 503);
+            this.btnLimparTexto.Name = "btnLimparTexto";
+            this.btnLimparTexto.Size = new System.Drawing.Size(75, 23);
+            this.btnLimparTexto.TabIndex = 6;
+            this.btnLimparTexto.Text = "&Limpar";
+            this.btnLimparTexto.UseVisualStyleBackColor = true;
+            this.btnLimparTexto.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFechar.Location = new System.Drawing.Point(637, 503);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(75, 23);
+            this.btnFechar.TabIndex = 7;
+            this.btnFechar.Text = "&Fechar";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.btnFecharTetxo_Click);
+            // 
+            // frmCalculosBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(991, 610);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.btnLimparTexto);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblSinal);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Name = "frmCalculosBot";
+            this.Text = "Cálculos com Botões";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.Label lblSinal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnSomar;
+        private System.Windows.Forms.Button btnLimparTexto;
+        private System.Windows.Forms.Button btnFechar;
+    }
+}
+
---
+++ Menu_Calculos/frmCalculosBot.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Menu_Calculos
+{
+    public partial class frmCalculosBot : Form
+    {
+        public frmCalculosBot()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lblSinal.Text = "?";
+            lblResultado.Text = "?";
+            txtN1.Clear();
+            txtN2.Clear();
+            txtN1.Focus();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnFecharTetxo_Click(object sender, EventArgs e)
+        {
+            Close(); // fecha a janela
+        }
+
+        private void txtN2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtN1.Text); // var numérica
+            double b = double.Parse(txtN2.Text);
+            lblSinal.Text = "+"; // muda o texto
+            lblResultado.Text = (a + b).ToString(); // precisa converter pois é esperado um text
+        }
+    }
+}
